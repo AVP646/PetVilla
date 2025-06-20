46feecc2c6135282aa8eddd7779dff50fb0aecad
AVP646 <avpcollection646@gmail.com>
2025-06-20 17:28:53 +0530

    aded search bar functionality

--- partial/_navbar.php
+++ partial/_navbar.php
@@ -7,33 +7,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
     <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-</style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
     <style>
+
         .nav-i i{
             padding:0 20px;
-            /* border:1px solid black; */
-            margin:0 10px;
+            margin:10px 10px;
 
         }
+        .nav-i input{
+            display:none;
+            transition:0.5 linear;
 
+        }
         .nav-i i:hover{
             font-size:18px;
             transition:0.4s ease , 0.3 ease;
         }
 
-        .nav-links li{
+         .nav-links li{
             font-family: "Poppins", sans-serif;
             font-style: normal;
             font-weight:500;
             padding:0 10px;
-            /* border:1px solid black; */
-        }
+         }
+        
         .nav-links li:hover{
             font-size:18px;
-            /* font-size:20px; */
-            transition:0.4s ease;
-        }
+             
+            transition:0.3s ease;
+        } 
         .nav-title{
             padding:0 0 0 30px;
             font-family: "Poppins", sans-serif;
@@ -44,12 +48,18 @@
             width:60px;
             transform:rotate(30deg);
         }
+        #cancel{
+          display:none;
+          transition:0.5 ease;
+        }
     </style>
 </head>
 <body>
+
+<!-- this is navabr and it work with bootstrap  -->
     <nav class="navbar navbar-expand-lg BG-COLOR ">
   <div class="container-fluid">
-    <a class="navbar-brand nav-title"  data-aos="fade-down" data-aos-duration="1000" href="#"><img src="../images/logo.png" alt="" class="nav-im" > PetVilla</a>
+    <a class="navbar-brand nav-title" href="index.php"><img src="../images/logo.png" alt="" class="nav-im" > PetVilla</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -58,7 +68,6 @@
         <li class="nav-item">
           <a data-aos="fade-down" data-aos-duration="1200" class="nav-link active  nav-links" aria-current="page" href="index.php">HOME</a>
         </li>
-        
          <li class="nav-item">
           <a data-aos="fade-down" data-aos-duration="1300" class="nav-link active nav-links" aria-current="page" href="about_us.php">ABOUT</a>
         </li>
@@ -71,20 +80,39 @@
         </li>
     </ul>
         
-
-      
       <form class="d-flex nav-i" role="search" id="form_nav">
-        <i class="fa-solid fa-magnifying-glass "></i>
-         <i class="fa-solid fa-cart-shopping"></i>
+        <input data-aos="fade-down" id="search-box" data-aos-duration="1600" class="form-control me-2 rounded-pill  " type="search" placeholder="Search" aria-label="Search"/>
+        <i class="fa-solid fa-xmark" id="cancel"></i>
+        <i class="fa-solid fa-magnifying-glass " id="search"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
         <i class="fa-solid fa-heart"></i>
         <i class="fa-solid fa-user"></i>
-        <!-- <input data-aos="fade-down" data-aos-duration="1600" class="form-control me-2 rounded-pill  " type="search" placeholder="Search" aria-label="Search"/> -->
-        <!-- <button data-aos="fade-down" data-aos-duration="1700" class="btn"  type="submit"><i class="fa-solid fa-magnifying-glass"></i></button> -->
       </form>
-       
     </div>
   </div>
 </nav>
+<!-- here its ends  -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+<script>
+
+ let icon = document.querySelector('#search');
+ let box = document.querySelector('#search-box');
+ let cancel = document.querySelector('#cancel');
+icon.addEventListener('click', function () {
+  this.style.display = 'none';
+  box.style.display = 'block';
+  cancel.style.display = 'block';
+  console.log("hello");
+});
+
+cancel.addEventListener('click',function () {
+  this.style.display = 'none';
+  box.style.display = 'none';
+  icon.style.display = 'block';
+  console.log("why");
+});
+
+</script>
 </body>
 </html>
