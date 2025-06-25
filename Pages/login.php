3e3f3054730877b08777eb522fddad861b757daf
AVP646 <avpcollection646@gmail.com>
2025-06-25 11:54:46 +0530

    r nad l done

---
+++ Pages/login.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/css/intlTelInput.min.css" />
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400..800&family=Poppins:wght@400;600&display=swap');
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            color-scheme: light dark;
+            font-family: "Baloo 2", cursive;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100vw;
+            height: 100vh;
+            flex-direction: column;
+            position: absolute;
+            top: 0;
+            background-color: rgba(170, 158, 158, 0.64);
+        }
+
+        .main-register-container {
+            width: 100%;
+            max-width: 600px;
+            height: auto;
+            display: flex;
+            justify-content: center;
+            background-color: rgba(209, 208, 208, 0.5);
+            box-shadow: 2px 2px 3px black;
+            border: 1px solid transparent;
+            border-radius: 5px;
+            flex-direction: column;
+            position: relative;
+            padding: 20px;
+        }
+
+        .min-box {
+            width: 200px;
+            height: 100px;
+            background-color: rgba(209, 208, 208, 0.5);
+            position: absolute;
+            right: 0;
+            top: 0;
+            border-bottom-left-radius: 50px;
+            border: 1px solid transparent;
+            box-shadow: -1px 2px 5px rgba(54, 53, 53, 0.64);
+        }
+
+        @media (max-width: 768px) {
+            .min-box {
+                width: 180px;
+                height: 100px;
+            }
+        }
+
+        @media (max-width: 576px) {
+            .min-box {
+                width: 110px;
+                height: 80px;
+            }
+        }
+
+        .register-logo {
+            font-size: 35px;
+            position: relative;
+            fill: transparent;
+            stroke: black;
+            stroke-dashoffset: 50;
+            stroke-dasharray: 50;
+            animation: scrollGradient 5s linear infinite;
+        }
+
+        @keyframes scrollGradient {
+            0% {
+                fill: black;
+            }
+
+            50% {
+                stroke-dashoffset: 0;
+            }
+
+            100% {
+                fill: black;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="main-register-container container">
+        <div class="min-box"></div>
+        <svg viewBox="400">
+            <text x="49" y="40" class="register-logo"> Login Form</text>
+        </svg>
+
+        <form action="./home.php" method="POST" class="w-100">
+
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <label class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control" required>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <label class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" required minlength="6" autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="row justify-content-center mb-3">
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-dark">Login</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
--- partial/_navbar.php
+++ partial/_navbar.php
@@ -1,131 +1,136 @@
-
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navabr</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-    </style>
-    <style>
-        *{
-          padding: 0;
-          margin: 0;
-        }
-        .nav-i i{
-            padding:0 20px;
-            margin:10px 10px;
-            transition:0.3s ease-in-out;
-           
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Navabr</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  </style>
+  <style>
+    * {
+      padding: 0;
+      margin: 0;
+    }
 
-        }
-        .nav-i input{
-            display:none;
-            transition:0.5 linear;
+    .nav-i i {
+      padding: 0 20px;
+      margin: 10px 10px;
+      transition: 0.3s ease-in-out;
+      color: black;
+      cursor: pointer;
+    }
 
-        }
-        .nav-i i:hover{
-            font-size:18px;
-            transition:0.4s ease , 0.3 ease;
-        }
+    .nav-i input {
+      display: none;
+      transition: 0.5 linear;
+      width: 180px;
+    }
 
-         .nav-links li{
-            font-family: "Poppins", sans-serif;
-            font-style: normal;
-            font-weight:500;
-            padding:0 10px;
-            transition:0.3s ease-in-out;
+    .nav-i i:hover {
+      font-size: 18px;
+      transition: 0.4s ease, 0.3 ease;
+    }
 
-         }
-        
-        .nav-links li:hover{
-            font-size:18px;
-             
-        } 
-        .nav-title{
-            padding:0 0 0 30px;
-            font-family: "Poppins", sans-serif;
-            font-style: normal;
-            font-weight:500;
-        }
-        .nav-im{
-            width:60px;
-            transform:rotate(30deg);
-        }
-         #cancel{
-          display:none;
-          transition:0.5 ease;
-        }
-        /* .bg-color{
+    .nav-links li {
+      font-family: "Poppins", sans-serif;
+      font-style: normal;
+      font-weight: 500;
+      padding: 0 10px;
+      transition: 0.3s ease-in-out;
+
+    }
+
+    .nav-links li:hover {
+      font-size: 18px;
+
+    }
+
+    .nav-title {
+      padding: 0 0 0 30px;
+      font-family: "Poppins", sans-serif;
+      font-style: normal;
+      font-weight: 500;
+    }
+
+    .nav-im {
+      width: 60px;
+      transform: rotate(30deg);
+    }
+
+    #cancel {
+      display: none;
+      transition: 0.5 ease;
+    }
+
+    /* .bg-color{
           background:url('images/slider-bg.png');
           z-index: 0;
         }  */
-    </style>
+  </style>
 </head>
 
 <body>
 
-<!-- this is navabr and it work with bootstrap  -->
-    <nav class="navbar navbar-expand-lg BG-COLOR ">
-  <div class="container-fluid">
-    <a class="navbar-brand d-flex align-items-center" href="index.php"><img src="../images/logo.png" alt="" class="nav-im" > PetVilla</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto nav-links ">
-        <li class="nav-item">
-          <a data-aos="fade-down" data-aos-duration="1200" class="nav-link active  nav-links" aria-current="page" href="../Pages/home.php">HOME</a>
-        </li>
-        <li class="nav-item">
-          <a data-aos="fade-down" data-aos-duration="1200" class="nav-link active  nav-links" aria-current="page" href="../Pages/home.php">PETS</a>
-        </li>
-        <li class="nav-item">
-          <a data-aos="fade-down" data-aos-duration="1400" class="nav-link active nav-links" aria-current="page" href="product.php">PRODUCT</a>
-        </li>
-        <li class="nav-item">
-         <a data-aos="fade-down" data-aos-duration="1300" class="nav-link active nav-links" aria-current="page" href="../Pages/about.php">ABOUT</a>
-       </li>
-         </li>
+  <!-- this is navabr and it work with bootstrap  -->
+  <nav class="navbar navbar-expand-lg BG-COLOR ">
+    <div class="container-fluid">
+      <a class="navbar-brand d-flex align-items-center" href="index.php"><img src="../images/logo.png" alt="" class="nav-im"> PetVilla</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto nav-links ">
           <li class="nav-item">
-          <a data-aos="fade-down" data-aos-duration="1500" class="nav-link active nav-links" aria-current="page" href="contactUs.php">CONTACT US</a>
-        </li>
-    </ul>
-        
-      <form class="d-flex nav-i " role="search" id="form_nav">
-        <input data-aos="fade-down" id="search-box" data-aos-duration="1600" class="form-control me-2 rounded-pill  " type="search" placeholder="Search" aria-label="Search"/>
-        <i class="fa-solid fa-xmark" id="cancel"></i>
-        <i class="fa-solid fa-magnifying-glass " id="search"></i>
-        <i class="fa-solid fa-cart-shopping"></i>
-        <i class="fa-solid fa-heart"></i>
-        <i class="fa-solid fa-user"></i>
-      </form>
+            <a data-aos="fade-down" data-aos-duration="1200" class="nav-link active  nav-links" aria-current="page" href="../Pages/home.php">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a data-aos="fade-down" data-aos-duration="1200" class="nav-link active  nav-links" aria-current="page" href="../Pages/home.php">PETS</a>
+          </li>
+          <li class="nav-item">
+            <a data-aos="fade-down" data-aos-duration="1400" class="nav-link active nav-links" aria-current="page" href="product.php">PRODUCT</a>
+          </li>
+          <li class="nav-item">
+            <a data-aos="fade-down" data-aos-duration="1300" class="nav-link active nav-links" aria-current="page" href="../Pages/about.php">ABOUT</a>
+          </li>
+          </li>
+          <li class="nav-item">
+            <a data-aos="fade-down" data-aos-duration="1500" class="nav-link active nav-links" aria-current="page" href="contactUs.php">CONTACT US</a>
+          </li>
+        </ul>
+
+        <form class="d-flex nav-i " role="search" id="form_nav">
+          <input data-aos="fade-down" id="search-box" data-aos-duration="1600" class="form-control me-2 rounded-pill  " type="search" placeholder="Search" aria-label="Search" />
+          <i class="fa-solid fa-xmark" id="cancel"></i>
+          <i class="fa-solid fa-magnifying-glass " id="search"></i>
+          <i class="fa-solid fa-cart-shopping"></i>
+          <i class="fa-solid fa-heart"></i>
+          <a href="../Pages/register.php"><i class="fa-solid fa-user"></i></a>
+        </form>
+      </div>
     </div>
-  </div>
-</nav>
-<!-- here its ends  -->
+  </nav>
+  <!-- here its ends  -->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-<script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  <script>
+    let icon = document.querySelector('#search');
+    let box = document.querySelector('#search-box');
+    let cancel = document.querySelector('#cancel');
+    icon.addEventListener('click', function() {
+      this.style.display = 'none';
+      box.style.display = 'block';
+      cancel.style.display = 'block';
+      console.log("hello");
+    });
 
- let icon = document.querySelector('#search');
- let box = document.querySelector('#search-box');
- let cancel = document.querySelector('#cancel');
-icon.addEventListener('click', function () {
-  this.style.display = 'none';
-  box.style.display = 'block';
-  cancel.style.display = 'block';
-  console.log("hello");
-});
-
-      cancel.addEventListener('click',function () {
-        box.style.display = 'none' ;
-        icon.style.display = 'block';
-        this.style.display = 'none';
-      });
+    cancel.addEventListener('click', function() {
+      box.style.display = 'none';
+      icon.style.display = 'block';
+      this.style.display = 'none';
+    });
   </script>
 
 </body>
