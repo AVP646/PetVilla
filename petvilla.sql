41ac467201655409a6dd402f539190dadc9a791b
AVP646 <avpcollection646@gmail.com>
2025-07-29 10:17:29 +0530

    done

--- petvilla.sql
+++ petvilla.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2025 at 02:59 PM
+-- Generation Time: Jul 29, 2025 at 06:46 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,6 +35,14 @@ CREATE TABLE `cart` (
   `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `user_id`, `product_id`, `product_type`, `quantity`) VALUES
+(15, 5, 1, 'pet', 1),
+(16, 5, 4, 'pet', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +63,13 @@ CREATE TABLE `food` (
   `food-description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`food_id`, `food-image`, `food-name`, `food-category`, `food-size`, `food-mfd`, `food-exd`, `food-instruction`, `food-availability`, `food-price`, `food-description`) VALUES
+(1, '../Products/pedigree.webp', 'pedigreeee', 'Dog', '10kg', '12/4/23', '12/4/29', 'per day 100gram', 'Available', 9999, 'rat divash suta pehla');
+
 -- --------------------------------------------------------
 
 --
@@ -68,6 +83,21 @@ CREATE TABLE `orders` (
   `total_amount` decimal(10,2) DEFAULT NULL,
   `payment_status` enum('pending','paid') DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `order_date`, `total_amount`, `payment_status`) VALUES
+(25, 5, '2025-07-25 19:13:16', 54999.00, 'pending'),
+(26, 5, '2025-07-26 09:14:44', 24999.00, 'pending'),
+(27, 5, '2025-07-26 10:17:47', 109995.00, 'pending'),
+(28, 5, '2025-07-27 10:55:02', 19998.00, 'pending'),
+(29, 5, '2025-07-27 11:00:43', 30000.00, 'pending'),
+(30, 5, '2025-07-27 11:04:56', 8999.00, 'pending'),
+(31, 5, '2025-07-27 11:05:31', 24999.00, 'pending'),
+(32, 5, '2025-07-28 08:48:16', 24999.00, 'pending'),
+(33, 5, '2025-07-28 20:12:10', 83996.00, 'pending');
 
 -- --------------------------------------------------------
 
@@ -83,6 +113,26 @@ CREATE TABLE `order_items` (
   `quantity` int(11) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`order_item_id`, `order_id`, `product_id`, `product_type`, `quantity`, `price`) VALUES
+(1, 25, 2, 'pet', 1, 30000.00),
+(2, 25, 1, 'pet', 1, 24999.00),
+(3, 26, 1, 'pet', 1, 24999.00),
+(4, 27, 1, 'pet', 4, 24999.00),
+(5, 27, 1, 'food', 1, 9999.00),
+(6, 28, 1, 'food', 2, 9999.00),
+(7, 29, 2, 'pet', 1, 30000.00),
+(8, 30, 3, 'pet', 1, 8999.00),
+(9, 31, 1, 'pet', 1, 24999.00),
+(10, 32, 1, 'pet', 1, 24999.00),
+(11, 33, 3, 'pet', 1, 8999.00),
+(12, 33, 1, 'pet', 1, 24999.00),
+(13, 33, 1, 'food', 2, 9999.00),
+(14, 33, 2, 'pet', 1, 30000.00);
 
 -- --------------------------------------------------------
 
@@ -103,6 +153,16 @@ CREATE TABLE `pets` (
   `pet-availability` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `pets`
+--
+
+INSERT INTO `pets` (`pets_id`, `pet-image`, `pet-name`, `pet-breed`, `pet-age`, `pet-gender`, `pet-category`, `pet-description`, `pet-price`, `pet-availability`) VALUES
+(1, '../Pets/dog1.jpg', 'Pitbull', 'American Pit Bull Terrier', '2 y', 'Male', 'Dog', 'The American Pit Bull Terrier is a dog breed recognized by the United Kennel Club and the American Dog Breeders Association, but not the American Kennel Club. It is a medium-sized, short-haired dog, o', 24999, 'Available'),
+(2, '../Pets/cate1.webp', 'Cuttie', 'British Shorthair', '3 M', 'Female', 'Cat', 'The British Shorthair is the pedigree version of the traditional British domestic cat, with a distinctively stocky body, thick coat, and broad face. The most familiar colour variant is the \"British Bl', 30000, 'Available'),
+(3, '../Pets/parraot1.jpg', 'mithhu', 'cockatiel', '2 m', 'Male', 'Bird', 'The cockatiel, also known as the weero/weiro or quarrion, is a medium-sizedparrot that is a member of its own branch of the cockatoo family endemic to Australia. They are prized as exotic household pe', 8999, 'Available'),
+(4, '../Pets/dog1.jpg', 'BullDog', 'PitBull', '3 M', 'Female', 'Dog', 'The American Pit Bull Terrier is a dog breed recognized by the United Kennel Club and the American Dog Breeders Association, but not the American Kennel Club. It is a medium-sized, short-haired dog, o', 23999, 'Sold');
+
 -- --------------------------------------------------------
 
 --
@@ -114,10 +174,19 @@ CREATE TABLE `users` (
   `fname` text NOT NULL,
   `lname` text NOT NULL,
   `email` varchar(50) NOT NULL,
-  `Mno` int(10) NOT NULL,
+  `Mno` varchar(10) NOT NULL,
   `password` varchar(255) NOT NULL,
   `username` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `fname`, `lname`, `email`, `Mno`, `password`, `username`) VALUES
+(5, '', '', 'pankajmm35@gmail.com', '8980441087', '$2y$10$OfJzduHEI8IvLZT3Ikt7refwOnX6UT2MBu61VQiwPmeUR/0ZA9BYm', 'pankaj@'),
+(8, 'dipu', 'pankaj mangniya', 'haha@gmail.com', '8980441087', '$2y$10$o4PwaPSXrAQz5KdGpN..re/m7kdBhW1eUtu6PvCJozQCSWhc6vf56', 'dipu@'),
+(9, 'ayush', 'r', 'haha@gmail.com', '333', '$2y$10$TppNrPVUgFNeno8TzI11aesh4wcwfUkNimRbqPw.ZQdsskXvo654K', 'ayush@');
 
 --
 -- Indexes for dumped tables
@@ -170,37 +239,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `food_id` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `food_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `pets`
 --
 ALTER TABLE `pets`
-  MODIFY `pets_id` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `pets_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
